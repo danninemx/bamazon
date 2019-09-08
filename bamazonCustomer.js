3bfa399b61d75a79b8c84d2f5df9c47fc03a119a
Danny Kim <dudkny@gmail.com>
2019-09-08 17:08:18 -0500

    Completed Customer View

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 // Global variables
 const mysql = require("mysql");
 const inquirer = require('inquirer');
+let started = false;
 
 // DB connection
 let connection = mysql.createConnection({
@@ -11,10 +12,31 @@ let connection = mysql.createConnection({
     database: "bamazon"
 })
 
+let farewell = function () {
+    console.log(`\nTHANK YOU FOR CHOOSING BAMAZON.  PLEASE COME AGAIN!\n\n\n`)
+    connection.end();
+}
+
+// // 
+// let pause = function () {
+//     return inquirer
+//         .prompt([
+//             {
+//                 type: 'confirm',
+//                 message: '***** PLEASE TRY AGAIN *****',
+//                 name: 'confirm'
+//             }])
+//         .then((con) => {
+//             if (!con.confirm) {
+//                 start();
+//             }
+//         })
+// }
+
 // Update DB with order details.
-let fulfillOrder = function (fi, fq, cu) {
-    let newQty = parseInt(cu) - parseInt(fq);
-    connection.query('UPDATE `products` SET ? WHERE item_id = ?',
+let fulfillOrder = async function (fi, fq, cu, pr) {
+    let newQty = cu - fq;
+    await connection.query('UPDATE `products` SET ? WHERE item_id = ?',
         [
             {
                 stock_quantity: newQty
@@ -22,71 +44,93 @@ let fulfillOrder = function (fi, fq, cu) {
             fi
         ],
         function (err, res) {
-
             if (err) throw err;
-            console.log(res.affectedRows + " product updated!\n");
+
+            let total = pr * fq;
+            console.log(`\n----------------------------\n`);
+            console.log(`${res.affectedRows} product updated!\n`);
+            console.log(`Your total is $${total}.\n`);
+            console.log(`----------------------------\n`);
             // console.log(res);
+            farewell();
         })
+    // // End DB conenction.
+    // connection.end();
 
-
-    // End DB conenction.
-    connection.end();
 }
 
 // Call this to check stock quantity.
 let checkStock = function (ci, cq) {
-    connection.query('SELECT item_id, stock_quantity FROM `products` WHERE item_id = ?', [ci], function (err, res) {
+    connection.query('SELECT item_id, price, stock_quantity FROM `products` WHERE item_id = ?', [ci], function (err, res) {
         if (err) throw err;
 
-        // If current stock quantity is sufficient, fulfill order. Restart otherwise.
+        // console.log(res);
+        let price = res[0].price;
         let curr = res[0].stock_quantity;
+        // console.log(price, ' ', curr);
+
+        // If current stock quantity is sufficient, fulfill order. Restart otherwise.
         if (curr > 0 && curr >= cq) {
-            fulfillOrder(ci, cq, curr)
+            fulfillOrder(ci, cq, curr, price)
         } else {
-            console.log(`\n*** Insufficient quantity! ***\n`);
+            console.log(`\n\n***** INSUFFICIENT AVAILABILITY (currently ${curr} in stock) *****\n`);
             takeOrder();
         }
     })
 }
 
-// Call this to let user place order.
+// Call this to start taking user's order.
 let takeOrder = function () {
-    console.log(`\n*** Placing order ***\n`);
+    console.log(`\n***** Please enter your order details *****\n`);
     return inquirer
         .prompt([
             {
                 type: 'input',
-                message: 'Please enter the ITEM ID of the item you wish to purchase.',
+                message: 'ITEM ID :',
                 name: 'id'
             },
             {
                 type: 'input',
-                message: 'How many units would you like to purchase?',
+                message: 'Quantity :',
                 name: 'quantity'
             }])
         .then((order) => {
-            let id = order.id;
+            // Validate quanity.
+            let id = order.id.toString().trim();
             let qty = parseInt(order.quantity);
-            checkStock(id, qty);
+            if (typeof qty === 'number' && qty > 0) {
+                checkStock(id, qty);
+            } else {
+                console.log(`\n***** Quantity has to be a positive integer. *****\n`);
+                // pause();
+                // Restart.
+                // start();
+                farewell();
+            }
         })
 }
 
-console.log(`*** Querying all products. ***\n`);
+// Call this to start customer interaction.
+let start = function () {
+    console.log(`***** Querying all products. *****\n`);
 
-// Query all items in "products" table.
-connection.query('SELECT item_id, product_name, price FROM `products`', function (err, res) {
-    if (err) throw err;
+    // Query all items in "products" table.
+    connection.query('SELECT item_id, product_name, price FROM `products`', function (err, res) {
+        if (err) throw err;
+        console.log(`***** ${res.length} matches found. *****\n`);
 
-    console.log(`*** ${res.length} matches found. ***\n`);
+        // Loop through the result and output each.
+        let count = 0;
+        for (ea of res) {
+            count++;
+            console.log(`${count}) ITEM ID: ${ea.item_id} || ITEM NAME: ${ea.product_name} || PRICE: $${ea.price}`);
+        }
+        takeOrder();
+    })
+}
 
-    // Loop through the result and output each.
-    let count = 0;
-    for (ea of res) {
-        count++;
-        console.log(`${count}) ITEM ID: ${ea.item_id} || ITEM NAME: ${ea.product_name} || PRICE: $${ea.price}`);
-    }
-
-    // console.log(`\n*** End of query. ***\n`);
-
-    takeOrder();
-});
+// Start on load.
+if (!started) {
+    started = true;
+    start();
+}
