d8cfa0e96323c2f25e65fd0889dfd2672a730d7b
Danny Kim <dudkny@gmail.com>
2019-09-08 12:18:57 -0500

    Implemented inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
-// GLOBAL VARIABLES
+// Global variables
 const mysql = require("mysql");
+const inquirer = require('inquirer');
 
+// DB connection
 let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -8,6 +10,27 @@ let connection = mysql.createConnection({
     password: "password",
     database: "bamazon"
 })
+
+let takeOrder = function () {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please enter the ITEM ID of the item you wish to purchase.',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'How many units would you like to purchase?',
+            name: 'quantity'
+        }])
+        .then((order) => {
+            let id = order.id.trim();
+            let qty = parseInt(order.quantity);
+
+            console.log(`ITEM ID = ${id}
+            QUANTITY = ${qty}`);
+        })
+}
 
 console.log(`*** Querying all products. ***\n`);
 
@@ -20,8 +43,13 @@ connection.query('SELECT item_id, product_name, price FROM `products`', function
     // Loop through the result and output each.
     let count = 0;
     for (ea of res) {
-        console.log(`${count}) ITEM ID: ${ea.item_id} || ITEM NAME: ${ea.product_name} || PRICE: $${ea.price}`);
         count++;
+        console.log(`${count}) ITEM ID: ${ea.item_id} || ITEM NAME: ${ea.product_name} || PRICE: $${ea.price}`);
     }
+
+    // console.log(`\n*** End of query. ***\n`);
+
+    takeOrder();
+
     connection.end();
 });
